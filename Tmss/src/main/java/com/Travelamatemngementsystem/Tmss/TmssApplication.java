a2c1c545c0760624c75ea8af731c07abb4dc36aa
MohamedAzeem98 <ravichandrandamodran817@gmail.com>
2025-08-09 13:38:03 +0530

    added Sourcecode

---
+++ Tmss/src/main/java/com/Travelamatemngementsystem/Tmss/TmssApplication.java
@@ -0,0 +1,13 @@
+package com.Travelamatemngementsystem.Tmss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmssApplication.class, args);
+	}
+
+}
